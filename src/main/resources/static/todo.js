67d0293f367266be7b4f5ebc7b09d32a1c30f942
Jimmy Hedman <jimmy.hedman@gmail.com>
2024-02-21 16:21:05 +0100

    Add functionallity to add todos

--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -1,6 +1,7 @@
 async function getTodos() {
     const response = await fetch("api/todos")
     const todos = await response.json()
+    document.getElementById("todolist").innerHTML = []
     todos.forEach((todo) => {
             let newA = document.createElement("a")
             newA.classList.add("list-group-item")
@@ -13,4 +14,27 @@ async function getTodos() {
     )
 }
 
-window.onload = getTodos()
+async function clicked(e) {
+    e.preventDefault()
+    let task = document.getElementById("task")
+    const response = await fetch("api/todos", {
+        method: "POST", headers: {
+            "Content-Type": "application/json",
+
+        },
+        body: JSON.stringify({"task": task.value})
+    })
+    const todo = await response.json()
+    console.log(todo)
+    getTodos()
+    task.value = ""
+}
+
+async function loaded() {
+    getTodos()
+    let taskForm = document.getElementById("taskForm")
+    console.log(taskForm)
+    taskForm.addEventListener("submit", clicked)
+}
+
+addEventListener("DOMContentLoaded", loaded);
